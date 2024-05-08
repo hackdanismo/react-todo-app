1a778475d314b8d85d91b65ae685b856fef04f4f
hackdanismo <office@justtoconfirm.co.uk>
2024-05-08 15:46:42 +0100

    feat: setup the environment variables and the code to connect to the Supabase PostgreSQL database and render the content of the example database table within the application

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Environment variables
+Create a `.env` file within the root of the application and add the `Supabase` credentials. When using `Vite`, the `Environment Variables` should start with `VITE_` and can be accessed using `import.meta.env.VITE_` in the front-end code.
+
+```
+VITE_SUPABASE_PROJECT=https://xxxxxxxxxxxxx.supabase.co
+VITE_SUPABASE_ANON_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+import { createClient } from "@supabase/supabase-js"
+
+// Use credentials from the .env file when using Vite for the application, .dotenv is not required
+const supabase = createClient(import.meta.env.VITE_SUPABASE_PROJECT, import.meta.env.VITE_SUPABASE_ANON_KEY)
+
 const App = () => {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    getCountries()
+  }, [])
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select()
+    setCountries(data)
+  }
+
   return (
     <>
       <h1>Todo App</h1>
+
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
     </>
   )
 }
