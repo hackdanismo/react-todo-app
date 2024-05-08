09da980286a1b501cfd155f871ec2a4eec7a10e4
hackdanismo <160902037+hackdanismo@users.noreply.github.com>
2024-05-08 15:58:55 +0100

    feat: connect to the Supabase PostgreSQL database table and pull data to render on the front-end (#1)
    * feat: install the @supabase/supabase-js package to act as a client library and provide an interface to connect to the Supabase database
    
    * feat: setup the environment variables and the code to connect to the Supabase PostgreSQL database and render the content of the example database table within the application
    
    * feat: update the application to connect to the database and render data from the todo table which is to be used by the application
    
    ---------
    
    Co-authored-by: hackdanismo <office@justtoconfirm.co.uk>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+import { createClient } from "@supabase/supabase-js"
+
+// Use credentials from the .env file when using Vite for the application, .dotenv is not required
+const supabase = createClient(import.meta.env.VITE_SUPABASE_PROJECT, import.meta.env.VITE_SUPABASE_ANON_KEY)
+
 const App = () => {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetchAllTodos()
+  }, [])
+
+  async function fetchAllTodos() {
+    const { data } = await supabase.from("todo").select()
+    setTodos(data)
+  }
+
   return (
     <>
       <h1>Todo App</h1>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.task}>{todo.task}</li>
+        ))}
+      </ul>
     </>
   )
 }
