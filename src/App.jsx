36a90b67f1e1604c1d6d11b3bf96c1081098cc17
hackdanismo <office@justtoconfirm.co.uk>
2024-06-03 15:19:27 +0100

    chore: fetch and display the tasks that have been created for the user that is signed-in based on their user uid value

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ const App = () => {
   const [email, setEmail] = useState("")
   // State to capture the password value entered into the sign up form
   const [password, setPassword] = useState("")
+  // State to store tasks from the database
+  const [tasks, setTasks] = useState([])
 
   // Function to handle when a user completes the sign in form
   const handleSignIn = async (e) => {
@@ -28,8 +30,10 @@ const App = () => {
       if (error) throw error
       // Store data in the state
       setUser(data.user)
+      // Fetch the tasks for the user that is signed-in
+      fetchTasks(data.user.id)
     } catch (error) {
-      console.log(error.message)
+      console.error(error.message)
     }
   }
 
@@ -41,8 +45,24 @@ const App = () => {
       setUser(null)
       setEmail("")
       setPassword("")
+      // Clear the state, clearing the tasks when the user signs-out
+      setTasks([])
     } catch (error) {
-      console.log(error.message)
+      console.error(error.message)
+    }
+  }
+
+  // Function to fetch tasks based on the User UID value
+  const fetchTasks = async (userUid) => {
+    try {
+      const { data, error } = await supabase
+        .from("tasks")
+        .select("*")
+        .eq("user_uid", userUid)
+      if (error) throw error
+      setTasks(data)
+    } catch (error) {
+      console.error("Error fetching tasks:", error.message)
     }
   }
 
@@ -50,7 +70,15 @@ const App = () => {
     <>
       {user ? (<button type="button" onClick={handleSignOut}>Sign Out</button>):(<div>Not Signed In</div>)}
       {user ? (
+        <>
         <div>+++ User is Logged In +++</div>
+        <ul>
+          {/* Map over the tasks for the user that has signed in */}
+          {tasks.map(task => (
+            <li key={task.id}>{task.title}: {task.notes}</li>
+          ))}
+        </ul>
+        </>
       ) : (
         /* Sign up / Sign in form shows if user is not signed in */
         <form onSubmit={handleSignIn}>
