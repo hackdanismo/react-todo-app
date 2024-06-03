3577758c409a15b3abd23be90c84d2d7c8de4f35
hackdanismo <office@justtoconfirm.co.uk>
2024-06-03 15:10:38 +0100

    chore: update the SQL query to include the user_id field in the tasks table that adds the user UID value for each user that adds a task to allow only user tasks to be displayed

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -1,9 +1,10 @@
- -- Create the table
+ -- Create the table with reference to the User UID from within the Supabase auth users table
  CREATE TABLE tasks (
    id SERIAL PRIMARY KEY,
    title text NOT NULL,
    notes text NOT NULL,
    completed BOOLEAN DEFAULT FALSE
+   user_uid UUID REFERENCES auth.users(id)
  );
  -- Insert some sample data into the table
- INSERT INTO tasks (title, notes, completed) VALUES ('Tidy the room', 'Tidy the room so it can be nice and organised.', FALSE);
+ INSERT INTO tasks (title, notes, completed) VALUES ('Tidy the room', 'Tidy the room so it can be nice and organised.', FALSE, "2b8ff76f-e829-43d9-a060-f8a963228dbb");
